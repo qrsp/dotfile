059f961f3ca60780ef8a570d91177d1ffcc5044f
qrspncpr <qrspncpr@gmail.com>
2024-02-20 07:53:15 +0800

    project

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -7,12 +7,13 @@ return {
       -- Set header
       dashboard.section.header.val = {}
       dashboard.section.buttons.val = {
-        dashboard.button('n', ' > New File', '<cmd>ene<CR>'),
+        dashboard.button('e', ' > New File', '<cmd>ene<CR>'),
         dashboard.button('f', '󰱼 > Find Files', '<cmd>Telescope find_files<CR>'),
-        dashboard.button("g", "󰷾 > Find text", ":Telescope live_grep <CR>"),
+        dashboard.button("g", "󰷾 > Grep Text", ":Telescope live_grep <CR>"),
         dashboard.button('r', ' > Find Recent Files', '<cmd>Telescope oldfiles<CR>'),
         dashboard.button('w', ' > Find Workspaces', '<cmd>Telescope workspaces<CR>'),
-        dashboard.button('s', ' > Load Session', '<cmd>SessionsLoad<CR>'),
+        dashboard.button('s', ' > Load Session', '<cmd>SessionManager load_current_dir_session<CR>'),
+        dashboard.button('l', ' > Load Last Session', '<cmd>SessionManager load_last_session<CR>'),
         dashboard.button('q', ' > Quit NVIM', '<cmd>qa<CR>'),
       }
       alpha.setup(dashboard.opts)
